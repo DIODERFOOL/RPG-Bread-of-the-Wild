2855364861c5a7feca260b50fa9109bc623a8920
CPU\Fabricio <fabriciof@sentrics.mx>
2018-05-02 23:02:48 -0500

    Optimización de performance

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,13 @@
-package botw;
+ package botw;
 
+import javax.swing.JFrame;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.awt.Dimension;
 
 public class Game extends Canvas implements Runnable {
 
@@ -13,9 +16,14 @@ public class Game extends Canvas implements Runnable {
 	private Thread thread;
 	private boolean runnig = false;
 
+	public int tickCount = 0;
+
 	private Handler h;
 
 	public static BufferedImage ss;
+
+	private BufferedImage persona = new BufferedImage(WIDTH, HEIGTH, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt)persona.getRaster().getDataBuffer()).getData();
 
 /*----------Main Menu Stuff--------------*/
 	private Menu menu;
@@ -66,31 +74,56 @@ public class Game extends Canvas implements Runnable {
 		long lastTime = System.nanoTime();
 		double amountOfTicks = 60.0;
 		double ns = 1000000000 / amountOfTicks;
-		double delta = 0;
+
+		double delta = 0;  
 		long timer = System.currentTimeMillis();
+		
 		int frames = 0;
+		int ticks = 0; 
+		
 		while(runnig) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
 			lastTime = now;
+			boolean shouldRender = true;
+
 			while(delta >= 1) {
+				ticks++;
 				tick();
 				delta--;
+				shouldRender = true;
 			}
-			if(runnig)
-				render();
-			frames ++;
 
-			if(System.currentTimeMillis() - timer > 1000) {
+			try{
+				Thread.sleep(2);
+			}catch (InterruptedException e){
+				e.printStackTrace();
+			}
+
+			if(shouldRender){
+				render();
+				frames ++;
+			}
+
+			//if(runnig)
+
+			if(System.currentTimeMillis() - timer >= 1000) {
 				timer += 1000;
-				System.out.println("FPS: " + frames);
+				System.out.println("FPS: " + frames + " TICKS(Updates): " + ticks);
 				frames = 0;
+				ticks = 0;
 			}
 		}
 		stop();
 	}
 
 	private void tick() {
+		tickCount++;
+
+		for(int i=0; i<pixels.length; i++){
+			pixels[i] = i+ tickCount;
+		}
+
 		if(State == STATE.GAME){
 			h.tick();
 		}
@@ -104,6 +137,8 @@ public class Game extends Canvas implements Runnable {
 		}	
 
 		Graphics g = bs.getDrawGraphics();
+
+		g.drawImage(persona, 0, 0, WIDTH, HEIGTH, null);
 
 		g.setColor(Color.black);
 		g.fillRect(0,0, WIDTH, HEIGTH);
